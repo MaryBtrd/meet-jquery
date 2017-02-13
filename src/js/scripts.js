417540809c623cf86ff3347019523f6737dd63e4
MaryBtrd <marie.bertrand@student.hepl.be>
2017-02-13 11:19:34 +0100

    Exercise part three

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,21 +3,30 @@
 /src/js/scripts.js-Main scripts
 *
 */
+let iTrombinoCurrentIndex = 0,
+    iTrombinoLength,
+    $TrombinoFigures;
+
 const fHandleTabClick = function( oEvent ) {
   oEvent.preventDefault();
   const $target = oEvent.currentTarget;
   if ( $target.parentNode.classList.contains( "active" ) ) {
     return;
-    console.log( "Tab is already active !");
   }
   document.querySelector( "ul.nav-tabs li.active" ).classList.remove( "active" );
   $target.parentNode.classList.add( "active" );
   document.querySelector( ".tab-content .tab-pane.active" ).classList.remove( "active" );
   document.getElementById($target.getAttribute( "data-tab-target" ) ).classList.add( "active" );
-  console.log("Tab clicked!" );
 
 };
-
+const fUpdateTrombino = function(){
+  $TrombinoFigures[ iTrombinoCurrentIndex ].classList.add( "hide" );
+  if( ++iTrombinoCurrentIndex === iTrombinoLength ){
+    iTrombinoCurrentIndex = 0;
+  }
+  $TrombinoFigures[ iTrombinoCurrentIndex ].classList.remove( "hide" );
+};
+ // When page is loaded
 window.addEventListener( "load", function(){
   //1. select a with rel=external attributes.
  Array.from(document.querySelectorAll( 'a[rel*="external"]' ) ).forEach( function( $elt ) {
@@ -30,4 +39,11 @@ window.addEventListener( "load", function(){
      $elt.addEventListener("click", fHandleTabClick);
    } );
 
+   //3. trombinoscope
+   document.querySelectorAll( "#trombino figure:not(:first-of-type)" ).forEach( function( $elt ){
+     $elt.classList.add( "hide" );
+   } );
+   $TrombinoFigures = Array.from(document.querySelectorAll("#trombino figure"));
+   iTrombinoLength = $TrombinoFigures.length;
+   setInterval( fUpdateTrombino, 1000 );
  } );
