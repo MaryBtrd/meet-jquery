407acd77e5a28d94f3ab4babaa6ce5a496c8933e
MaryBtrd <marie.bertrand@student.hepl.be>
2017-02-13 10:53:57 +0100

    Exercise part two

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,15 +3,31 @@
 /src/js/scripts.js-Main scripts
 *
 */
+const fHandleTabClick = function( oEvent ) {
+  oEvent.preventDefault();
+  const $target = oEvent.currentTarget;
+  if ( $target.parentNode.classList.contains( "active" ) ) {
+    return;
+    console.log( "Tab is already active !");
+  }
+  document.querySelector( "ul.nav-tabs li.active" ).classList.remove( "active" );
+  $target.parentNode.classList.add( "active" );
+  document.querySelector( ".tab-content .tab-pane.active" ).classList.remove( "active" );
+  document.getElementById($target.getAttribute( "data-tab-target" ) ).classList.add( "active" );
+  console.log("Tab clicked!" );
+
+};
 
 window.addEventListener( "load", function(){
   //1. select a with rel=external attributes.
- Array.from(document.querySelectorAll( 'a[rel*="external"]' ) ).forEach( function( $elt ){
-   $elt.addEventListener ("click", function( oEvent ) {
+ Array.from(document.querySelectorAll( 'a[rel*="external"]' ) ).forEach( function( $elt ) {
      $elt.setAttribute( "target", "_new" );
-    ;
+
+   } );
+
+   //2. tab mecanism
+   Array.from( document.querySelectorAll( "ul.nav.nav-tabs a" ) ).forEach( function( $elt ) {
+     $elt.addEventListener("click", fHandleTabClick);
    } );
 
  } );
-
-})
