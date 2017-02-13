75a0141ce8a92e238c158832e6c3e43ddef32f64
MaryBtrd <marie.bertrand@student.hepl.be>
2017-02-13 12:14:13 +0100

    Exercise one and two

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,21 @@
 /*Mary/Mett-jQuerry
 
 */
+const fChangeTab = function( oEvent ) {
+  oEvent.preventDefault();
+
+  $tabs.parent().filter( ".active" ).removeClass( "active" );
+  $( this ).parent().addClass ( "active" );
+  $( ".tab-content .tab-pane.active" ).removeClass( "active" );
+  $( `#${ $( this ).data( "tab-target" )}` ).addClass( "active" );
+};
+
+//called when DOM is ready.
+$( function () {
+
+  //1. link with rel=external
+  $( 'a[rel*="external"]' ).Attr( "target", "_new");
+
+  //2. tab
+  ( $tabs = $( "ul.nav.nav-tabs a" )).on( "click", fChangeTab );
+} );
